2792c094c45363591f4e0b903b5b7a490e6708c1
jdh5451 <jdh5451@g.rit.edu>
2019-10-25 12:29:28 -0400

    Basic Turret and Grid implementation
    Changes
    --Grid added, tiles are 1x1
    --Turret now snaps to the center of the nearest grid tile when placed in a valid location
    --Turret currently disables its drag and drop when placed in a valid location
    --Turrets cannot be placed in a grid tile that is occupied by another turret
    --Turrets autofire bullets at a rate of one per second when they have been placed in a valid spot
    --Turrets now have rigidbodies, larger colliders
    --Bullets added
    --Bullets have simple behavior wherein they travel forward (up in 2d space) automatically and despawn after a short time
    
    Next Todo:
    --Add Enemies
    --Add firing condition for turrets (targeting radius?)
    --Add directional firing for turrets

---
+++ Multi-Genre-Game/Assets/Geek/Turret Defense/Scripts/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public int speed;
+    public int lifespan;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 3;
+        lifespan = 3;
+        Destroy(this.gameObject, lifespan);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+}
--- Multi-Genre-Game/Assets/Geek/Turret Defense/Scripts/Turret.cs
+++ Multi-Genre-Game/Assets/Geek/Turret Defense/Scripts/Turret.cs
@@ -5,24 +5,33 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
     public Camera cam;
+    public Grid grid;
+    public GameObject bullet;
     Vector3 Position = new Vector3();
     float myLeft = 0;
     float myTop = 0;
     float myWidth = 200;
     float myHeight = 50;
 
+    int counter;//this will be replaced with a timer
+
 
     bool isDragging = true;
+    bool placeable = true;
+    bool isPlaced = false;
     //a bool to show if it's curently active or not
 
     // Start is called before the first frame update
     void Start()
     {
+        counter = 60;
         cam = Camera.main;
+        grid = GameObject.Find("Grid").GetComponent<Grid>();
         Position = (Camera.main.ScreenToWorldPoint(Input.mousePosition));
         Position.z = 1.0f;
         gameObject.transform.position = Position;
         Debug.Log("pos: "+gameObject.transform.position);
+        
     }
 
     // Update is called once per frame
@@ -32,6 +41,9 @@ public class Turret : MonoBehaviour
         if (Input.GetMouseButtonUp(0))
         {
             isDragging = false;
+            if (placeable==true)
+                SnapToGrid();
+            else Debug.Log("This tile is occupied!");
         }
 
         if (isDragging)
@@ -40,19 +52,56 @@ public class Turret : MonoBehaviour
             Position.z = 1.0f;
             gameObject.transform.position = Position;
         }
+
+        if (isPlaced)
+        {
+            //add stuff here for what the turret should do while it is active!
+            //add a radius of detection for stuff
+            //make it spawn a projectile
+            //something like that
+            if (counter == 60)
+            {
+                Instantiate(bullet, transform.position, Quaternion.identity);
+                counter = 0;
+            }
+            else
+                counter++;
+        }
     }
 
     //move the turret after it's been placed
     public void OnMouseDown()
     {
-        isDragging = true;
+        if(isPlaced)
+            isDragging = true;
 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        placeable = false;
+        gameObject.GetComponent<SpriteRenderer>().sortingOrder = 1;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        placeable = true;
+        gameObject.GetComponent<SpriteRenderer>().sortingOrder = 0;
     }
     //need a method to snap turret placement to grid
     //check if turret's placement is valid
     //if turret is not placed validly, move it back to where it was?
     //if turret is valid placement, now it can be put into play
 
+    /// <summary>
+    /// This method causes the game object to snap to the 
+    /// </summary>
+    public void SnapToGrid()
+    {
+        gameObject.transform.position = grid.GetCellCenterWorld(grid.WorldToCell(Position));
+        gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
+        isPlaced = true;
+    }
     //then we can focus on what the turret does once it's placed
     
 }
